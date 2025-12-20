de33aa1c6e151177bee1d7a2715a6336fceb9f0b
Francisco <franciscoocontatoo@gmail.com>
2025-12-19 22:31:11 -0300

    feat: Creating the ProductRepository

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Data;
 using Microsoft.Data.SqlClient;
 using ProductionSystem.Data;
+using ProductionSystem.Models;
+using ProductionSystem.Repositories;
 
 try
 {
@@ -11,8 +13,45 @@ try
             Console.WriteLine("Connection established with success!");
         }
 
+        Product product = new Product{
+            Code = "PROD-004",
+            ProductName = "Mesa Escritório",
+            ProductDescription = "Mesa de Escritório Premium",
+            UOM = "UN"
+        };
+
+        ProductRepository productRepository = new ProductRepository();
+        productRepository.InsertProduct(product);
+
+        Console.WriteLine($"The product was inserted with success {product.ProductName}");
+        Console.WriteLine("All products in the database: ");
+        
+        List<Product> products = productRepository.GetAllProducts();
+
+        foreach(var value in products)
+        {
+            Console.WriteLine($"ID: {value.ProductId} | Code: {value.Code} | Name: {value.ProductName}");
+        }
+
+        Console.WriteLine("Getting only the product that has ID equal to 3.");
+        Product productThree = productRepository.GetProductById(3);
+
+        if (productThree != null)
+        {
+            Console.WriteLine("Product three found it!");
+            Console.WriteLine($"ID: {productThree.ProductId}");
+            Console.WriteLine($"Code: {productThree.ProductName}");
+            Console.WriteLine($"Name: {productThree.ProductName}");
+            Console.WriteLine($"Description: {productThree.ProductDescription}");
+            Console.WriteLine($"UOM: {productThree.UOM}");
+        } else
+        {
+            Console.WriteLine("Produto não encontrado!");
+        }
+        
+
     }
-    
+
     Console.WriteLine("Connection closed");
 
 }
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using ProductionSystem.Data;
+using ProductionSystem.Models;
+
+namespace ProductionSystem.Repositories
+{
+    public class ProductRepository
+    {
+        public void InsertProduct(Product product)
+        {
+            // 1. First open the connection database 
+            using (SqlConnection connection = DatabaseConnection.GetConnection())
+            {
+                // 2. Create the SQL command
+                string query = "INSERT INTO Products (Code, ProductName, ProductDescription, UOM) " + "VALUES (@Code, @ProductName, @ProductDescription, @UOM)";
+
+                // 3. Pass as a params
+                SqlCommand command = new SqlCommand(query, connection);
+
+                // 4. Values by params
+                command.Parameters.AddWithValue("@Code", product.Code);
+                command.Parameters.AddWithValue("@ProductName", product.ProductName);
+                command.Parameters.AddWithValue("@ProductDescription", product.ProductDescription);
+                command.Parameters.AddWithValue("@UOM", product.UOM);
+
+                // 5. Executing and passing the values
+                command.ExecuteNonQuery();
+
+            }
+
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            using (SqlConnection connection = DatabaseConnection.GetConnection())
+            {
+                string query = "SELECT * FROM Products";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Product product = new Product
+                    {
+                        ProductId = (int)reader["ProductId"],
+                        Code = reader["Code"].ToString(),
+                        ProductName = reader["ProductName"].ToString(),
+                        ProductDescription = reader["ProductDescription"].ToString(),
+                        UOM = reader["UOM"].ToString()
+                    };
+
+                    products.Add(product);
+                }
+
+            }
+
+            return products;
+        }
+
+        public Product GetProductById(int productId)
+        {
+            using (SqlConnection connection = DatabaseConnection.GetConnection())
+            {
+                string query = "SELECT * FROM Products WHERE ProductId = @ProductId";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ProductId", productId);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    Product product = new Product
+                    {
+                        ProductId = (int)reader["ProductId"],
+                        Code = reader["Code"].ToString(),
+                        ProductName = reader["ProductName"].ToString(),
+                        ProductDescription = reader["ProductDescription"].ToString(),
+                        UOM = reader["UOM"].ToString()
+                    };
+
+                    return product;
+                }
+
+                return null;
+
+            }
+
+        }
+    }
+}
