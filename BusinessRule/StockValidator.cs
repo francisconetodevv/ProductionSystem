50d4c3b4c3158beb7f740156d2ca67d288a39208
Francisco <franciscoocontatoo@gmail.com>
2025-12-24 00:17:11 -0300

    feat: Creating the bussiness value behind the production order

--- BusinessRule/StockValidator.cs
+++ BusinessRule/StockValidator.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProductionSystem.Models;
+using ProductionSystem.Repositories;
 
 namespace ProductionSystem.BusinessRule
 {
     public class StockValidator
     {
+        private RawMaterialRepository _rawMaterialRepository = new RawMaterialRepository();
 
+        public StockValidationResult CheckStock(List<ProductionOrderRawMaterials> materials)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
